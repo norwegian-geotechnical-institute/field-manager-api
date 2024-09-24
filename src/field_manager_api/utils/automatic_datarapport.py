811098f02ee9d385e0438f4cbd6e7af76d29ddd9
Tellef Kydland <tellef.kydland@ngi.no>
2024-09-24 09:30:22 +0200

    adding a test and removeing some unused functionimports

--- src/field_manager_api/utils/automatic_datarapport.py
+++ src/field_manager_api/utils/automatic_datarapport.py
@@ -5,7 +5,7 @@ import folium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from field_manager_api.methods.get_methods import get_methods, Method
+from field_manager_api.methods.get_methods import Method
 
 
 class WordDocReport:
---
+++ tests/test_fieldmanager_api.py
@@ -0,0 +1,14 @@
+import pytest
+from field_manager_api.field_manager import FieldManagerAPI
+
+
+def test_set_token_permission_error():
+    fm = FieldManagerAPI()
+    token = "your_invalid_token"  # Use an invalid or expired token
+    fm.set_token(token)
+
+    # Ensure that the PermissionError is raised when fm.get_projects is called
+    with pytest.raises(
+        PermissionError, match="Token authorization failed \(401 Unauthorized\)"
+    ):
+        fm.get_projects()
